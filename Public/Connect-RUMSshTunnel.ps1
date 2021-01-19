f007c79c28da9d062a52d4b2174fc76c2b38e752
Mikael Ekberg <mikael@lakitu.co>
2021-01-19 17:10:51 +0100

    Change handling of SSH tunnels on Linux

--- Public/Connect-RUMSshTunnel.ps1
+++ Public/Connect-RUMSshTunnel.ps1
@@ -21,12 +21,30 @@ function Connect-RUMSshTunnel {
     )
 
     if (Test-Connection -TargetName $TunnelComputerName -TCPPort $TunnelPort -Quiet -ErrorAction SilentlyContinue) {
-        $Forwarding = "$($LocalPort):$($RemoteHostName):$($RemoteHostPort)"
-        $Command = "ssh $TunnelComputerName -l $UserName -p $TunnelPort -L $Forwarding -N"
-        $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Command)
-        $EncodedCommand = [Convert]::ToBase64String($Bytes)
+        
+        switch ($true) {
+            $IsWindows { 
+                $Forwarding = "$($LocalPort):$($RemoteHostName):$($RemoteHostPort)"
+                $Command = "ssh $TunnelComputerName -l $UserName -p $TunnelPort -L $Forwarding -N"
+                $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Command)
+                $EncodedCommand = [Convert]::ToBase64String($Bytes)
 
-        Start-Process -FilePath pwsh -ArgumentList "-EncodedCommand $EncodedCommand"
+                Start-Process -FilePath pwsh -ArgumentList "-EncodedCommand $EncodedCommand"
+            }
+            $IsMacOS {
+                Write-Error "MacOS is not supported. Yet..." -ErrorAction Stop
+                return
+            }
+            $IsLinux {
+                $Forwarding = "$($LocalPort):$($RemoteHostName):$($RemoteHostPort)"
+                ssh $TunnelComputerName -l $UserName -p $TunnelPort -L $Forwarding -f sleep 30
+            }
+            Default {
+                Write-Error "Could not identify operating system, aborting." -ErrorAction Stop
+                return
+            }
+        }
+        
     }
     else {
         Write-Error "A connection with the server [$TunnelComputerName] on port [$TunnelPort] could not be established." -ErrorAction Stop
